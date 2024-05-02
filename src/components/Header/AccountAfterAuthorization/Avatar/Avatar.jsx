1393452e3866f40ded67b4a892e83c2a8b568c37
Alexander <tverdokhlebalex@me.com>
2024-05-02 18:49:38 +0400

    Update Avatar.jsx

--- src/components/Header/AccountAfterAuthorization/Avatar/Avatar.jsx
+++ src/components/Header/AccountAfterAuthorization/Avatar/Avatar.jsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import {useNavigate} from 'react-router-dom';
-import avatar from '../../../../media/Avatar/avatar.svg'
-import './Avatar.css';
-import { signOut } from '../../../../requests/profile';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import avatar from "../../../../media/Avatar/avatar.svg";
+import "./Avatar.css";
+import { signOut } from "../../../../requests/profile";
+import { useDispatch } from "react-redux";
 function ExitButton() {
   const navigate = useNavigate();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   function handleClick() {
     navigate("/login");
     dispatch(signOut());
-  } 
+  }
   return (
-    <button className='avatar__button' type="button" onClick={handleClick}>
+    <button className="avatar__button" type="button" onClick={handleClick}>
       Выйти
     </button>
   );
 }
 const Avatar = () => {
   return (
-    <div className='avatar'>
-      <div className='avatar__content'>
-        <span>Алексей К.</span>
+    <div className="avatar">
+      <div className="avatar__content">
+        <span>Алексей А.</span>
         <ExitButton />
       </div>
-      <div className='avatar__img'>
-        <img src={avatar} alt='avatar'/>
+      <div className="avatar__img">
+        <img src={avatar} alt="avatar" />
       </div>
     </div>
-  )
+  );
 };
 
 export default Avatar;
